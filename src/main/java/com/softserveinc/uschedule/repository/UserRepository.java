8525c4c2ed42be0aaa0b36e445ec5f19a3b49a2f
vlishch <genre@genre-VirtualBox>
2016-06-23 13:56:07 +0300

    Add initial UserRepository

---
+++ src/main/java/com/softserveinc/uschedule/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.softserveinc.uschedule.repository;
+
+
+import com.softserveinc.uschedule.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
