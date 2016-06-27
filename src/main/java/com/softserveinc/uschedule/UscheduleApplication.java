a1784fc49e1f0844e41f07882799b3b6e4b384e2
npu3pak0406 <static-theonly@rambler.ru>
2016-06-27 18:25:56 +0300

    working example spring + angularjs

--- src/main/java/com/softserveinc/uschedule/UscheduleApplication.java
+++ src/main/java/com/softserveinc/uschedule/UscheduleApplication.java
@@ -1,19 +1,11 @@
 package com.softserveinc.uschedule;
 
-import com.softserveinc.uschedule.entity.Room;
-import com.softserveinc.uschedule.repository.RoomRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class UscheduleApplication {
-
-    @PostConstruct
-    public void init() {
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(UscheduleApplication.class, args);
