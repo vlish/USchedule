9f16a2e50b87f4d2ed5f81784821de23d9987ad7
npu3pak0406 <static-theonly@rambler.ru>
2016-07-01 13:43:06 +0300

    remove scope provider

--- src/main/java/com/softserveinc/uschedule/controller/MainController.java
+++ src/main/java/com/softserveinc/uschedule/controller/MainController.java
@@ -1,14 +1,9 @@
 package com.softserveinc.uschedule.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * Created by ostash on 27.06.2016.
- */
 @Controller
 public class MainController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
